6b02ec3f70787707b753ff44d1d5df47d7d5a4d7
Adi Luhung Suryadi <adiluhung@gmail.com>
2015-12-25 23:21:53 +0800

    build setup

---
+++ src/containers/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './App';
+
+const Root = React.createClass({
+  render() {
+    return (
+        <App />
+    );
+  }
+});
+
+export default Root;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/dist',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      },
+      exclude: /node_modules/
+    }]
+  }
+};
